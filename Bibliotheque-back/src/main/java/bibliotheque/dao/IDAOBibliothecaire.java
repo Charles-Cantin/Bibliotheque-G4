2f04a35cae08c0fb41d473711c4755200c6eeb42
Nabil Nouira <nouiranabil@hotmail.fr>
2023-01-05 13:03:47 +0100

    controller rest bibliothecaire

---
+++ Bibliotheque-back/src/main/java/bibliotheque/dao/IDAOBibliothecaire.java
@@ -0,0 +1,9 @@
+package bibliotheque.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import bibliotheque.model.Bibliothecaire;
+
+public interface IDAOBibliothecaire extends JpaRepository<Bibliothecaire,Integer>{
+
+}
--- Bibliotheque-back/src/main/java/bibliotheque/model/Bibliothecaire.java
+++ Bibliotheque-back/src/main/java/bibliotheque/model/Bibliothecaire.java
@@ -3,17 +3,26 @@ package bibliotheque.model;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import bibliotheque.model.Views.ViewBase;
+
 @Entity
 @DiscriminatorValue("bibliothecaire")
 public class Bibliothecaire extends Compte{
+		
+	
+	@JsonView(ViewBase.class)
 	private String nom;
+	@JsonView(ViewBase.class)
 	private String prenom;
 	
 	
 	public Bibliothecaire() {
 	}
 	
-	
+
 
 	public String getNom() {
 		return nom;
