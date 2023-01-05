136a32fabb93e7fc472228fd1a547bc70bbdaf34
Charles_C <charles.cantin@protonmail.com>
2023-01-05 10:14:02 +0100

    bootification

---
+++ Bibliotheque-back/src/main/java/bibliotheque/BibliothequeBackApplication.java
@@ -0,0 +1,13 @@
+package bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeBackApplication.class, args);
+	}
+
+}
