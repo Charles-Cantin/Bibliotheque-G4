fa44e645219f5c593ab35a6a11c4a96cc241392c
Charles_C <charles.cantin@protonmail.com>
2023-01-13 12:38:12 +0100

    fixed lol

--- bibliotheque-front/src/app/app-routing.module.ts
+++ bibliotheque-front/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionComponent } from './page-connexion/inscription/inscription.component';
+import { PageConnexionComponent } from './page-connexion/page-connexion.component';
 
 const routes: Routes = [
   {path: "", component: PageConnexionComponent, pathMatch: 'full'},
-];
-  path: "inscription", component: InscriptionComponent, pathMatch: 'full'}];
+  {path: "inscription", component: InscriptionComponent, pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
