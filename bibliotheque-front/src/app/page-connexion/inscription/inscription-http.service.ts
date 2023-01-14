49bd53dda0a2aecdde4c0f57b742ddb85fe1d4bd
Charles_C <charles.cantin@protonmail.com>
2023-01-14 09:37:26 +0100

    inscription also connects

--- bibliotheque-front/src/app/page-connexion/inscription/inscription-http.service.ts
+++ bibliotheque-front/src/app/page-connexion/inscription/inscription-http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppConfigService } from 'src/app/app-config.service';
+import { GlobalVariablesService } from 'src/app/global-variables.service';
 import { Compte } from 'src/model';
 
 @Injectable({
@@ -12,14 +13,18 @@ export class InscriptionHttpService {
   
   serviceUrl: string;
 
-  constructor(private http: HttpClient, private appConfig: AppConfigService, private router: Router) {
+  constructor(private http: HttpClient, private appConfig: AppConfigService, private router: Router, private globalVariables: GlobalVariablesService) {
     this.serviceUrl = appConfig.backEndUrl + "comptes/inscription/";
    }
 
   create(formCompte: Compte) {
     this.http.post<Compte>(this.serviceUrl, formCompte).subscribe(resp => {
-      
-      this.router.navigate(['bibliothecaire']);
+      this.globalVariables.compteConnecte = resp;
+      switch (resp.type) {
+        case 'admin': alert("connection admin ok ; mais pas encore page admin"); this.router.navigate(['']) ; break;
+        case 'bibliothecaire': this.router.navigate(['bibliothecaire']) ; break;
+        case 'inscrit': this.router.navigate(['lecteur']) ; break;
+      }
     })
   }
 }
