1155da53fa1863e799e86c730637f1098528288c
Charles_C <charles.cantin@protonmail.com>
2023-01-15 17:05:08 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- bibliotheque-front/src/app/app-routing.module.ts
+++ bibliotheque-front/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { PageBibliothecaireComponent } from './page-bibliothecaire/page-bibliothecaire.component';
+import { ConnexionComponent } from './page-connexion/connexion/connexion.component';
 import { InscriptionComponent } from './page-connexion/inscription/inscription.component';
 import { PageLecteurComponent } from './page-lecteur/page-lecteur.component';
 import { PageEditionComponent } from './page-edition/page-edition.component';
@@ -8,7 +10,8 @@ import { PageConnexionComponent } from './page-connexion/page-connexion.componen
 
 const routes: Routes = [
   {path: "", redirectTo: "livre", pathMatch: 'full'},
-  {path: "inscription", redirectTo: "connexion"},
+  {path: "inscription", component: InscriptionComponent},
+  {path: "home", component: HomeComponent},
   {path: "connexion", component: PageConnexionComponent},
   {path: "bibliothecaire", component: PageBibliothecaireComponent},
   {path: "lecteur", component: PageLecteurComponent},
