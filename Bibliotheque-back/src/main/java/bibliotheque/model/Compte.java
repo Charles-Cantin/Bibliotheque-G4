99cf32f4e4f7d969347d375cc2a1524176dd9dec
Charles_C <charles.cantin@protonmail.com>
2023-01-18 16:12:02 +0100

    Nom Prenom pour tous Comptes

--- Bibliotheque-back/src/main/java/bibliotheque/model/Compte.java
+++ Bibliotheque-back/src/main/java/bibliotheque/model/Compte.java
@@ -12,6 +12,9 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+import bibliotheque.model.Views.ViewBase;
+
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -42,6 +45,12 @@ public abstract class Compte {
 	@Column(nullable=false)
 	protected String password;
 
+	@JsonView(ViewBase.class)
+	private String nom;
+	@JsonView(ViewBase.class)
+	private String prenom;
+	
+	
 	public Compte() {}
 
 	public String getLogin() {
@@ -70,6 +79,22 @@ public abstract class Compte {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 
 }
--- Bibliotheque-back/src/main/java/bibliotheque/model/Inscrit.java
+++ Bibliotheque-back/src/main/java/bibliotheque/model/Inscrit.java
@@ -14,12 +14,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 @Entity
 @DiscriminatorValue("inscrit")
 public class Inscrit extends Compte{
-	
-	@JsonView(Views.ViewBase.class)
-	private String nom;
-
-	@JsonView(Views.ViewBase.class)
-	private String prenom;
 
 	@JsonView(Views.ViewBase.class)
 	private LocalDate naissance;
@@ -37,18 +31,6 @@ public class Inscrit extends Compte{
 	public Inscrit() {
 	}
 
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
 	public LocalDate getNaissance() {
 		return naissance;
 	}
