e7928e4a840fa74de2fdbb6f99a055d33ec485fc
Charles_C <charles.cantin@protonmail.com>
2023-01-19 13:26:16 +0100

    Edge case: long names in the button

--- bibliotheque-front/src/app/navbar/navbar.component.ts
+++ bibliotheque-front/src/app/navbar/navbar.component.ts
@@ -11,12 +11,15 @@ import { AuthService } from '../auth.service';
 export class NavbarComponent {
 
   connectedAccount: Compte;
+  nomPrenom: string = "";
+  initials: string = "";
 
   constructor(protected authS: AuthService, private router: Router, private changeDetectorRef: ChangeDetectorRef){
     // Met à jour la var locale connectedAccount à chaque changement de route; 
     this.router.events.subscribe((ev) => {
       if (ev instanceof NavigationEnd) {
         this.connectedAccount = authS.getLoggedInAccount();
+       this.nomPrenom = reduceNomPrenom(this.connectedAccount.nom, this.connectedAccount.prenom);
       }
     });
   }
@@ -30,3 +33,9 @@ export class NavbarComponent {
 
   redirectMonCompte = this.authS.redirectMonCompte;
 }
+ function onlyCapitalLetters (myString:string) {
+  return myString.replace(/[^A-Z]+/g, "");
+}
+
+const reduceNomPrenom = (nom:string, prenom:string, maxLength:number=30) => {return (nom + " " + prenom).length < maxLength ? initials(prenom) + " " + nom : initials(prenom) + " " + initials(nom)} ;
+const initials = (str:string) => str.split('').filter(a => a.match(/[A-Z]/)).map(item => item + '.').join('');
